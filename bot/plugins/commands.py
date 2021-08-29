1e15c3a8135fe73a8e7964e9d09c6a718a17f364
Akashvbf <87358439+Akashvbf@users.noreply.github.com>
2021-08-29 11:29:15 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'OUR CHANNEL', url="https://t.me/MOVIE_MARKET_CHANNEL"
                                 )
                         ]
                     ]
