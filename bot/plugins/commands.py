6c7e9a73aff04c9ee21be2c324ad27d5760e30a6
HACKERsSPOtr <ccgbhhh4@gmail.com>
2024-08-18 13:26:32 +0100

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    'JOIN ALL MOVIE WORLD', url="https://t.me/allmovve"
                                 )
                         ]
                     ]
